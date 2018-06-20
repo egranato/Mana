25163ae75c5755046f1a81ca5bf2b3d0e57b0d5a
Ethan Granato <ethan@granato.net>
2018-06-20 10:47:52 -0600

    updated roll function

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -12,10 +12,14 @@ export class GameService {
   public roll(): number {
     return this.getRoll();
   }
+  public getRandom(min: number, max: number): number {
+    return Math.floor(Math.random()*(max-min+1)+min);
+  }
   private getRoll(): number {
-    const possibilities = [1, 2, 3, 4, 5, 6];
-    const rounds = Math.floor((((Math.random() * (Math.random() * 3000)) / Math.random())) / Math.random());
-    const result = rounds % possibilities.length;
-    return possibilities[result];
+   const rolls = [1, 2, 3, 4, 5, 6];
+   const playerBonusses = [4, 5, 6];
+   rolls.concat(playerBonusses);
+   const index = this.getRandom(0, rolls.length - 1);
+   return rolls[index];
   }
 }
